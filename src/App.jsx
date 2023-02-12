bfef61e63f29656c8bc202aa503e9b7c1767a560
Vikas Gupta <vg733834@gmail.com>
2023-02-13 01:18:22 +0530

    g you

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home></Home>}></Route>
       <Route path='/about' element={<About></About>}></Route>
+      
     </Routes>
     </>
    );
